1e2a8e1a6ed31c7fd7623afa9174a2e0adfc73b3
safarora <safal.arora@knowlarity.com>
2019-04-13 18:51:57 +0530

    collect static changes

--- vconnect_mailling/mailing/methods.py
+++ vconnect_mailling/mailing/methods.py
@@ -1,4 +1,3 @@
 def get_data():
-    return {'persons': [{"name": "safal", "value": 100}, {"name": "arora", "value": 100},
-                      {"name": "gd", "value": 100}, {"name": "safi", "value": 100},
-                      {"name": "gds", "value": 100}]}
+    return {"persons": [{"name": "safal", "value": 100}, {"name": "arora", "value": 100},
+                        {"name": "gd", "value": 100}, {"name": "safi", "value": 100}, {"name": "gds", "value": 100}]}
